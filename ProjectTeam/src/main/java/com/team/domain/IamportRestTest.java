ad9c50d10404e14121316e1a45e2ef7963550ea5
kimGiGong <kim961234@daum.net>
2022-09-28 12:59:52 +0900

    commit한다

---
+++ ProjectTeam/src/main/java/com/team/domain/IamportRestTest.java
@@ -0,0 +1,31 @@
+package com.team.domain;
+
+import com.siot.IamportRestClient.IamportClient;
+
+public class IamportRestTest {
+	
+
+	// variable
+	private String api_key = "4565286812547616";
+	private String api_secret = "rwecWl1uRkZXcYODaio6woHxjTfJlPhkSjlMWBEkXUChS4Ra7ir7gQMT2rOolu91PV7iVF6CMEMhw8ua";
+	//	get/set
+	public String getApi_key() {
+		return api_key;
+	}
+
+	public void setApi_key(String api_key) {
+		this.api_key = api_key;
+	}
+
+	public String getApi_secret() {
+		return api_secret;
+	}
+
+	public void setApi_secret(String api_secret) {
+		this.api_secret = api_secret;
+	}
+
+	
+	
+   
+}
