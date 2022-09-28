d2d39a561235bbe646ef66fa3480f167ad8af030
tjoeun <tjoeun@DESKTOP-BKDM4HR>
2022-09-28 17:37:15 +0900

    9/28 프로젝트 시작

---
+++ goldDog/src/main/java/com/goldDog/serivece/memberService.java
@@ -0,0 +1,20 @@
+package com.goldDog.serivece;
+
+import com.goldDog.domain.MemberVO;
+
+public interface memberService {
+
+	// 회원 가입 (추가)
+		public int addMember(MemberVO member);
+		// 권한 추가
+		public int addAuth(String auth, String id);
+		// 회원 정보 가져오기
+		public MemberVO getMember(String id);
+		// 회원 정보 수정 처리
+		public int ModifyMember(MemberVO member);
+		// 회원 탈퇴 처리
+		public int deleteMember(MemberVO member);
+	
+	
+	
+}
---
+++ goldDog/src/main/java/com/goldDog/serivece/memberServiceImpl.java
@@ -0,0 +1,48 @@
+package com.goldDog.serivece;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.goldDog.domain.MemberVO;
+import com.goldDog.persistence.MemberMapper;
+
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+public class memberServiceImpl implements memberService{
+	
+	
+	@Autowired MemberMapper mapper;
+
+	@Override	
+	public int addMember(MemberVO member) {
+		
+		return mapper.addMember(member);
+	}
+
+	@Override
+	public int addAuth(String auth, String id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public MemberVO getMember(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int ModifyMember(MemberVO member) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteMember(MemberVO member) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
